9435d4397e6ede1fb07dab527c92984b5f912d12
Daniya <daniyakowser89@gmail.com>
2022-03-09 15:10:06 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+//Creating a class user to store the data;
+class User {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  User({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+//Applying get request.
+
+  Future<List<User>> getRequest() async {
+    //replace your restFull API here.
+    String url = "https://jsonplaceholder.typicode.com/posts";
+    final response = await http.get(Uri.parse(url));
+
+    var responseData = json.decode(response.body);
+
+    //Creating a list to store input data;
+    List<User> users = [];
+    for (var singleUser in responseData) {
+      User user = User(
+          id: singleUser["id"],
+          userId: singleUser["userId"],
+          title: singleUser["title"],
+          body: singleUser["body"]);
+
+      //Adding user to the list.
+      users.add(user);
+    }
+    return users;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Http Get Request."),
+          leading: Icon(
+            Icons.api,
+          ),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: FutureBuilder(
+            future: getRequest(),
+            builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (ctx, index) => DataTable(
+                    columnSpacing: 50,
+                    columns: const [
+                      DataColumn(
+                          label: Text(
+                            'ID',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.pinkAccent),
+                          ),
+                          tooltip: 'represents first name of the user'),
+                      DataColumn(
+                          label: Text(
+                            'Code',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.pinkAccent),
+                          ),
+                          tooltip: 'represents code  of the user'),
+                      DataColumn(
+                          label: Text(
+                            'Details',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.pinkAccent),
+                          ),
+                          tooltip: 'represents details of the user'),
+                    ],
+                    rows: [
+                      DataRow(cells: [
+                        DataCell(
+                          Text(
+                            snapshot.data[index].userId.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            snapshot.data[index].id.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            snapshot.data[index].userId.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ])
+                    ],
+                    // title: Text(snapshot.data[index].id.toString()),
+                    // subtitle: Text(snapshot.data[index].userId.toString()),
+                    // contentPadding: EdgeInsets.only(bottom: 20.0),
+                    // leading: Text(snapshot.data[index].title),
+                  ),
+                );
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
